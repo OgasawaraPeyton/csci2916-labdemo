da7e9f3d6d28f9d7329f553139e728e23875f871
Peyton Ogasawara <pogasawara1@my.hpu.edu>
2019-04-26 13:15:55 -1000

    New method and Helper

--- hello.java
+++ hello.java
@@ -1,1 +1,11 @@
-hi
+public class java {
+    
+Public static void greeting(msg){
+	System.out.println(“Semester is almost over.”);
+	}
+
+public static void main(String[] args){
+    System.out.println("hello");
+    }   
+}
+
